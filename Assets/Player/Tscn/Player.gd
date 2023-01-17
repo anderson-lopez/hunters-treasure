07837da17cd8d5fd28faeb468a66cb21249dd4ca
Anderson Lopez <lopezanderson069@gmail.com>
2023-01-17 15:54:00 -0500

    proceso de refactorizacion completado

--- Assets/Player/Tscn/Player.gd
+++ Assets/Player/Tscn/Player.gd
@@ -21,7 +21,6 @@ func _ready():
 
 func _process(_delta):
 	motion_ctrl()
-	direction_ctrl()
 	jump_ctrl()
 	attack_ctrl()
 
@@ -40,8 +39,12 @@ func motion_ctrl():
 		
 		if get_axis().x == 0:
 			playback.travel("Idle")
-		else:
+		elif get_axis().x == 1:
 			playback.travel("Run")
+			$Sprite.flip_h = false
+		elif get_axis().x == -1:
+			playback.travel("Run")
+			$Sprite.flip_h = true
 		
 		match playback.get_current_node():
 			"Idle":
@@ -49,30 +52,20 @@ func motion_ctrl():
 				$Particles.emitting = false
 			"Run":
 				$Particles.emitting = true
-		
-		if get_axis().x == 1:
-			$Sprite.flip_h = false
-		elif get_axis().x == -1:
-			$Sprite.flip_h = true
+	
+	match $Sprite.flip_h:
+		true:
+			$RayCast.scale.x = -1
+		false:
+			$RayCast.scale.x = 1
 			
 	motion = move_and_slide(motion, FLOOR)
-
-#Control de Direccion
-func direction_ctrl():
-	match $Sprite.flip_h:
-		true: 
-			$RayWall.cast_to.x = -CAST_WALL
-			$RayEnemy.cast_to.x = -CAST_ENEMY
-		false:
-			$RayWall.cast_to.x = CAST_WALL
-			$RayEnemy.cast_to.x = CAST_ENEMY
 
 # Control de Salto
 func jump_ctrl():
 	match is_on_floor():
 		true:
 			can_move = true
-			$RayWall.enabled = false
 			
 			if Input.is_action_just_pressed("Jump"):
 				$Sounds/Jump.play()
@@ -80,31 +73,33 @@ func jump_ctrl():
 			
 		false:
 			$Particles.emitting = false
-			$RayWall.enabled = true
 			
 			if motion.y < 0:
 				playback.travel("Jump")
 			else:
 				playback.travel("Fall")
 			
-			if $RayWall.is_colliding():
+			if $RayCast/RayWall.is_colliding():
 				can_move = false
 				
-				var col = $RayWall.get_collider()
+				var body = $RayCast/RayWall.get_collider()
 				
-				if col.is_in_group("Wall") and Input.is_action_just_pressed("Jump"):
-					$Sounds/Jump.play()
-					motion.y -= JUMP_HEIGHT
-					
-					if $Sprite.flip_h: 
-						motion.x += BOUNCING_JUMP
-						$Sprite.flip_h = false
-					else:
-						motion.x -= BOUNCING_JUMP
-						$Sprite.flip_h = true
+				if body.is_in_group("Wall"):
+					if Input.is_action_just_pressed("Jump"):
+						$Sounds/Jump.play()
+						motion.y -= JUMP_HEIGHT
+						
+						if $Sprite.flip_h: 
+							motion.x += BOUNCING_JUMP
+							$Sprite.flip_h = false
+						else:
+							motion.x -= BOUNCING_JUMP
+							$Sprite.flip_h = true
 
 # Control de Ataques
 func attack_ctrl():
+	var body = $RayCast/RayHit.get_collider()
+	
 	if is_on_floor():
 		if get_axis().x == 0 and Input.is_action_just_pressed("Attack"):
 			match playback.get_current_node():
@@ -117,8 +112,12 @@ func attack_ctrl():
 				"Attack-2":
 					playback.travel("Attack-3")
 					$Sounds/Sword.play()
+			
+			if $RayCast/RayHit.is_colliding():
+				if body.is_in_group("Enemy"):
+					body.damage_ctrl()
 	else:
-		if get_axis().y != 0 and Input.is_action_just_pressed("Attack"):
+		if Input.is_action_just_pressed("Attack"):
 			match playback.get_current_node():
 				"Jump":
 					playback.travel("Air-Attack-1")
@@ -135,12 +134,3 @@ func attack_ctrl():
 					playback.travel("Air-Attack-2")
 					$Sounds/Sword.play()
 	
-	if playback.get_current_node() == "Attack-1" or playback.get_current_node() == "Attack-2" or playback.get_current_node() == "Attack-3":
-		$RayEnemy.enabled = true
-	else:
-		$RayEnemy.enabled = false
-	
-	var col = $RayEnemy.get_collider()
-	
-	if $RayEnemy.is_colliding() and col.is_in_group("Enemy"):
-		col.queue_free()
